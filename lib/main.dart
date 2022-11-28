60d4c78cc0fc46bbc3c8cd4b190a8452c2578e46
Priyank-Bhagat <priyankb4dev@proton.me>
2022-11-28 10:46:44 +0530

    Disabled Landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'input_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const BmiCalculator());
 }
 
